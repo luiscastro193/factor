c6c9faeab9d8558d5276ecabfac5d99db004b07e
Luis Castro Martín <luiscastro193@gmail.com>
2024-09-23 21:39:52 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -19,19 +19,8 @@ factorElement.addEventListener('change', setFactor);
 gr.addEventListener('input', () => ml.value = fix(gr.value / factor));
 ml.addEventListener('input', () => gr.value = fix(ml.value * factor));
 
-function selectInput(input) {
-	setTimeout(function() {
-		try {
-			input.setSelectionRange(0, input.value.length);
-		}
-		catch(e) {
-			input.select();
-		}
-	}, 0);
-}
-
 for (let input of [gr, ml]) {
 	input.disabled = false;
 	input.addEventListener('mouseenter', () => input.focus());
-	input.addEventListener('focus', () => selectInput(input));
+	input.addEventListener('focus', () => input.select());
 }
