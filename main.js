fba3471ec76a5a17381340430c134501a5f56a96
luiscastro193 <luiscastro193@gmail.com>
2024-09-26 20:22:28 +0200

    Value as number

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const ml = document.getElementById('ml');
 let factor;
 
 function setFactor() {
-	let values = factorElement.value.split('/');
-	[gr.value, ml.value] = values;
+	let values = factorElement.value.split('/').map(Number);
+	[gr.valueAsNumber, ml.valueAsNumber] = values;
 	factor = values[0] / values[1];
 }
 
@@ -16,8 +16,8 @@ function fix(value) {
 
 setFactor();
 factorElement.addEventListener('change', setFactor);
-gr.addEventListener('input', () => ml.value = fix(gr.value / factor));
-ml.addEventListener('input', () => gr.value = fix(ml.value * factor));
+gr.addEventListener('input', () => ml.valueAsNumber = fix(gr.valueAsNumber / factor));
+ml.addEventListener('input', () => gr.valueAsNumber = fix(ml.valueAsNumber * factor));
 
 for (let input of [gr, ml]) {
 	input.disabled = false;
